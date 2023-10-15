79a37992dddfee26a3cd94c187c5e2009400d468
criskell <cris.moraes.ds@gmail.com>
2023-10-15 18:27:16 -0300

    feat(graphql): get more information from errors

--- packages/server/src/graphql/makeGraphQlHttpOptions.ts
+++ packages/server/src/graphql/makeGraphQlHttpOptions.ts
@@ -6,4 +6,10 @@ export const makeGraphQlHttpOptions = (): Options => ({
   schema,
   pretty: true,
   graphiql: true,
+  customFormatErrorFn: (error) => ({
+    message: error.message,
+    locations: error.locations,
+    stack: error.stack ? error.stack.split("\n") : [],
+    path: error.path,
+  }),
 });
