4d7a125894c12c2d2f1e07de7083200404591925
criskell <cris.moraes.ds@gmail.com>
2023-10-16 22:04:16 -0300

    feat(mongoose): add productModel

---
+++ packages/server/src/modules/product/productModel.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, InferSchemaType, Schema } from "mongoose";
+
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
+  priceInCents: {
+    type: Number,
+    required: true,
+  },
+});
+
+export type ProductDocument = InferSchemaType<typeof ProductSchema> & Document;
+
+export const ProductModel = mongoose.model<ProductDocument>(
+  "Product",
+  ProductSchema
+);
