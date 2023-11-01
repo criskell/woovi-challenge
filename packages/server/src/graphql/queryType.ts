7f82670138eb24b0852ded2d7ebd59dd72a23246
criskell <cris.moraes.ds@gmail.com>
2023-10-31 22:08:30 -0300

    feat(products): add products connection field

--- packages/server/src/graphql/queryType.ts
+++ packages/server/src/graphql/queryType.ts
@@ -1,11 +1,14 @@
 import { GraphQLObjectType, GraphQLString } from "graphql";
+
 import { nodeField, nodesField } from "./typeRegistry";
+import { productConnectionField } from "../modules/product/productFields";
 
 export const queryType = new GraphQLObjectType({
   name: "Query",
   fields: () => ({
     node: nodeField,
     nodes: nodesField,
+    ...productConnectionField,
     hello: {
       type: GraphQLString,
       resolve() {
