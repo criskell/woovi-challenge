04237bde34925bcaa9173e1bc1bfb9ff8fe2b76f
criskell <96352451+criskell@users.noreply.github.com>
2023-11-14 18:51:49 -0300

    chore(testing): test createProductMutation (#25)

--- packages/server/src/modules/product/__tests__/createProductMutation.spec.ts
+++ packages/server/src/modules/product/__tests__/createProductMutation.spec.ts
@@ -1,5 +1,47 @@
-it("hello", () => {
-  console.log("hello");
+import { graphql } from "graphql";
 
-  expect(1).toBe(1);
+import {
+  clearDatabase,
+  connectWithMongoose,
+  disconnectWithMongoose,
+} from "../../../../test";
+import { schema } from "../../../graphql/schema";
+import { ProductModel } from "../productModel";
+
+beforeAll(connectWithMongoose);
+beforeEach(clearDatabase);
+afterAll(disconnectWithMongoose);
+
+describe("CreateProductMutation", () => {
+  it("should create a new product", async () => {
+    const mutation = `
+      mutation CreateProductMutation($input: createProductInput!) {
+        createProduct(input: $input) {
+          product {
+            id
+          }
+        }
+      }
+    `;
+
+    const input = {
+      name: "example",
+      description: "example product",
+      priceInCents: 100,
+    };
+
+    await graphql({
+      schema,
+      source: mutation,
+      variableValues: { input },
+    });
+
+    const products = await ProductModel.find();
+
+    expect(products.length).toBe(1);
+
+    const productCreated = products[0];
+
+    expect(productCreated.toObject()).toEqual(expect.objectContaining(input));
+  });
 });
